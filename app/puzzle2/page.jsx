2a287faa37d9b34c6841691b3c0389b7801f5626
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-07-14 15:13:39 +0200

    Redirect user to next riddle upon pressing "next" button

--- app/puzzle2/page.jsx
+++ app/puzzle2/page.jsx
@@ -1,9 +1,14 @@
 'use client'
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 
-export default function(){
+export default function EscapeRoom2() {
   const [clue, setClue] = useState('');
   const [showSolution, setShowSolution] = useState(false);
+  const [clientLoaded, setClientLoaded] = useState(false);
+
+  useEffect(() => {
+    setClientLoaded(true);
+  }, []);
 
   const handleClueChange = (event) => {
     setClue(event.target.value);
@@ -14,11 +19,16 @@ export default function(){
       setShowSolution(true);
     }
   };
-    return(
-        <div>
-            <h1>Escape-Room Rätsel 2</h1>
+
+  const redirectUser = () => {
+    window.location.href = '/puzzle3';
+  };
+
+  return (
+    <div>
+      <h1>Escape-Room Rätsel 2</h1>
       <p>Willkommen im zweiten Raum. Um zu entkommen, musst du das Rätsel lösen.</p>
-      <br/>
+      <br />
       {!showSolution ? (
         <div>
           <p>Finde das richtige Wort und tippe es hier ein:</p>
@@ -28,9 +38,11 @@ export default function(){
       ) : (
         <div>
           <p>Herzlichen Glückwunsch! Du hast das Rätsel gelöst!</p>
-          <p>Die Tür öffnet sich und du gehst in den Raum 3.</p>
+          {clientLoaded && (
+            <button onClick={redirectUser}>Zum Raum 3</button>
+          )}
         </div>
       )}
-        </div>
-    )
+    </div>
+  );
 }
