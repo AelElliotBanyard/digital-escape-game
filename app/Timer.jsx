5ff5ac93011bab177a6d5ff1c1a8a457de9cb43f
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-07-14 15:11:01 +0200

    Timer added to global layout

---
+++ app/Timer.jsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+function Timer() {
+  const [time, setTime] = useState(null);
+  const [countdown, setCountdown] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    let timerId;
+    if(countdown && time > 0){
+      timerId = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+    else if(time === 0){
+      router.push('/failed');
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [time, countdown]);
+
+  const startCountdown = () => {
+    setTime(10);
+    setCountdown(true);
+  };
+
+  return (
+    <div>
+      {time !== null && <p>Time remaining: {time} seconds</p>}
+      <button onClick={startCountdown}>Start countdown</button>
+    </div>
+  );
+}
+
+export default Timer;
